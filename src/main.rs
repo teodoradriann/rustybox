6b5db24982d9ef2b338413fe37f215da72e12c6e
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2023-10-17 14:53:50 +0300

    update tests repository

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-/* Suggestions: 1. Fait une fonction pour traiter chaque commande
-                2. Commencer avec l'implementation de la commande pwd
-                3. Continuer avec les commandes simples qui ont pas des autres parametres
-                4. Implementez la logique pour traiter les commandes recu de la ligne de commande dans main, a l'aide d'un match (vous pouvez voir 
-                des exemples dans les solutions du TP1), mais c'est pas obligatoire
+/* Suggestions: 1. Write a function for every command
+                2. Start with the pwd command
+                3. Continue with the other commands that do not have parameters
 */
 
 fn pwd() {
 
-    // TODO 3: Implementez la logique de pwd ici
+    // TODO 3: Implement the logic for pwd
 
 }
 
 fn main() {
 
-    // TODO 1: Prend ici les arguments de la ligne de commande
+    // TODO 1: Read the command line arguments
 
-    // TODO 2: Si la commande est pwd, appeller pwd(); fait la meme chose pour les autres commandes
-
+    // TODO 2: If the first argument is pwd, call pwd()
 }
